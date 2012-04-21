6dcddb487edca83f7452a4b9a18f812f28a85bb3
Aviv Ben-Yosef <aviv.by@gmail.com>
2012-04-21 20:19:06 +0300

    a bit clean up

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,15 +2,16 @@ class TransactionsController < ApplicationController
   def index
     @name = params[:id]
     wanted_sector = params.fetch(:sector) { nil }
+    @txns = user_txns
     @sectors = Transaction.sectors_for_user(@name)
-    if wanted_sector
-      @txns = Transaction.for_user_and_sector(@name, wanted_sector)
-    else
-      @txns = Transaction.for_user(@name)
-    end
 
     @this_month = Transaction.sum_for_month(@name, wanted_sector, Time.now)
     @prev_month = Transaction.sum_for_month(@name, wanted_sector, Time.now - 1.month)
     @avg_month = Transaction.avg_per_month(@name, wanted_sector)
+  end
+
+  private
+  def user_txns
+    Transaction.for_user(@name).sector(params.fetch(:sector) { nil })
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,16 +1,11 @@
 class Transaction < ActiveRecord::Base
   attr_accessible :amount, :date, :sector, :supplier_name, :address
 
+  scope :sector, lambda { |sector| sector ? where("sector = ?", sector) : all }
+  scope :for_user, lambda { |user| where("user_id = ?", user) }
+
   def self.sectors_for_user(user)
     find(:all, :conditions => [ "user_id = ?",  user ], :select => "distinct sector").map(&:sector)
-  end
-
-  def self.for_user(user)
-    where(:user_id => user)
-  end
-
-  def self.for_user_and_sector(user, sector)
-    where(:user_id => user, :sector => sector)
   end
 
   def self.sum_for_month(user, sector, month_date)
